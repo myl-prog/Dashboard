70827b1d684ad516b8b82ea0e6e151c830d3b5ae
yassine <mohamedyassinelguirati@gmail.com>
2023-07-23 23:17:48 +0200

    Navbar (+ Sidebar dynamicity through EventListener 'resize' )

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -10,10 +10,18 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children} ) => {
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]:
+        true});
+    };
     const [activeMenu, setActiveMenu] = useState(true);
     return (
         <StateContext.Provider value={{
-            activeMenu,setActiveMenu
+            activeMenu,setActiveMenu,isClicked,
+            setIsClicked,handleClick,
+            screenSize, setScreenSize
         }}>
             {children}
         </StateContext.Provider>
